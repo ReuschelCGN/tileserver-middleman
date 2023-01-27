e54f647578153d911528e075b8f6bdc8f231cee6
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2023-01-27 21:16:19 +0100

    Update middleman.py

--- middleman.py
+++ middleman.py
@@ -107,8 +107,13 @@ def point_to_lat(lat_center, lon_center, zoom, width, height, wanted_points):
     # copied from https://help.openstreetmap.org/questions/75611/transform-xy-pixel-values-into-lat-and-long
     C = (256 / (2*math.pi)) * 2**zoom
 
-    xcenter = C * (math.radians(int(float(lon_center))) + math.pi)
-    ycenter = C * (math.pi - math.log(math.tan((math.pi/4) + math.radians(int(float(lat_center))) / 2)))
+    if isinstance(lon_center, str):
+        lon_center = int(float(lon_center))
+    if isinstance(lat_center, str):
+        lat_center = int(float(lat_center))
+
+    xcenter = C * (math.radians(lon_center) + math.pi)
+    ycenter = C * (math.pi - math.log(math.tan((math.pi/4) + math.radians(lat_center) / 2)))
 
     xpoint = xcenter - (width / 2 - wanted_points[0])
     ypoint = ycenter - (height / 2 - wanted_points[1])
